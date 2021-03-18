432bfcf3f06c3c0e7198003ab1be7d20230dd017
Natalie Rojas <natalie.rojas113@gmail.com>
2021-03-18 15:36:13 -0400

    FE: top view chart

--- app/http/web/app/src/App.js
+++ app/http/web/app/src/App.js
@@ -1,11 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import SingleResult from "./components/resultsComponent/SingleResult";
 
 class App extends React.Component {
   constructor(props) {
-    super(props)
-    this.handleSubmit = this.handleSubmit.bind(this)
+    super(props);
+    this.state = {
+      result: {},
+      loaded: false,
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
   async handleSubmit(e) {
     //axios.post("/handleUpload")
@@ -22,29 +27,43 @@ class App extends React.Component {
     // })
     //   .then(res => res.json())
     //   .then(res => console.log(res));
-    console.log(e.target)
+    console.log(e.target);
     const data = new FormData();
-    data.append('submission',e.target[0].files[0] );
-    const res = await fetch('http://127.0.0.1:3145/handleUpload', {
-      method: 'POST',
+    data.append("submission", e.target[0].files[0]);
+    const res = await fetch("http://127.0.0.1:3145/handleUpload", {
+      method: "POST",
       headers: {
-        'Access-Control-Allow-Origin': '*',
+        "Access-Control-Allow-Origin": "*",
       },
-      body: data
-    })
+      body: data,
+    });
     var jsonRes = await res.json();
     console.log(jsonRes);
+    this.setState({ loaded: true });
+    this.setState({ result: jsonRes });
   }
 
-  render(){ 
+  render() {
     return (
       <div className="App">
         <header className="App-header">
-          <form action="/handleUpload" method="post" encType="multipart/form-data" onSubmit={this.handleSubmit}>
-              Choose the file: <input type="file" name="submission"/> <br />
-              <input type="submit" value="Upload"/>
-          </form> 
+          <form
+            action="/handleUpload"
+            method="post"
+            encType="multipart/form-data"
+            onSubmit={this.handleSubmit}
+          >
+            Choose the file: <input type="file" name="submission" /> <br />
+            <input type="submit" value="Upload" />
+          </form>
         </header>
+        <div>
+          {this.state.loaded ? (
+            <SingleResult result={this.state.result} />
+          ) : (
+            <div>No data yet</div>
+          )}
+        </div>
       </div>
     );
   }
