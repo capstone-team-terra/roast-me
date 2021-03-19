d4680bc035433b06de53ab54da0e6b008118f4f7
yvonneshui <yvonne.shui126@gmail.com>
2021-03-19 15:51:55 -0400

    FE:components structure set

---
+++ app/http/web/app/src/components/resultsComponent/AllResults.js
@@ -0,0 +1,31 @@
+import React from "react";
+import ShowsCount from "./ShowsCount";
+
+export class AllResults extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      result: {},
+      loaded: false,
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ result: this.props.result, loaded: true });
+  }
+
+  render() {
+    if (!this.state.loaded) {
+      return "No results available at this moment!";
+    }
+    console.log("allresults state---->", this.state);
+    return (
+      <div>
+        <h2>All Results</h2>
+        <ShowsCount result={this.state.result.views} />
+      </div>
+    );
+  }
+}
+
+export default AllResults;
