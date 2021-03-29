fb3d07f758eab3ce99c3aef0d04a13c501ad46d1
alisasu <alisa.su97@gmail.com>
2021-03-29 02:21:25 -0400

    test

---
+++ src/config.js
@@ -0,0 +1,13 @@
+import firebase from "firebase/app";
+import "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_APIKEY,
+  authDomain: process.env.FIREBASE_AUTHDOMAIN,
+  projectId: process.env.FIREBASE_PROJECTID,
+  storageBucket: process.env.FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDERID,
+  appId: process.env.FIREBASE_APPID,
+  measurementId: process.env.FIREBASE_MEASUREMENTID,
+};
+export const app = firebase.initializeApp(firebaseConfig);
