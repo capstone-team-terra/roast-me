2010bb7423fdc11cee28d9299d393a6d06e45cb9
Natalie Rojas <natalie.rojas113@gmail.com>
2021-03-31 18:21:21 -0400

    FE: Github logo/link added

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import React from "react";
 import WelcomePage from "./components/WelcomePage";
 import { Switch, Route } from "react-router-dom";
 import LogIn from "./components/LogIn";
+import GitHub from "./GitHub"
 
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="App-header">
+      <GitHub />
       <Switch>
           <Route path="/results/:user">
             <LogIn />
@@ -16,7 +18,7 @@ const App = () => {
             <WelcomePage />
           </Route>
       </Switch>
-      </header>
+      </div>
     </div>
   );
 };
