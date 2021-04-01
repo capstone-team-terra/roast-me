c085334b26d4f04cda7cba3006495e530a4b9555
Natalie Rojas <natalie.rojas113@gmail.com>
2021-04-01 11:05:00 -0400

    FE: footer updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ export default function Footer(props) {
   return (
     <Container id="footer">
         <p className="mr-3">
-        A project by <strong>DANY</strong></p>
+        By Alisa Su, Danny Serrano, Natalie Rojas, Yizhou Shui</p>
         <p>
         Data questions ? <a href="#" onClick={props.handleDisclaimer} style={{textDecoration: 'underline', color: 'gray'}}>click here</a></p>
       </Container>
