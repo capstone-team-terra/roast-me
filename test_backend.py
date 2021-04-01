a8e438be4bd0ef726916c585b070968b58792818
alisasu <alisa.su97@gmail.com>
2021-04-01 16:59:12 -0400

    BE: tests for popularity

--- test_backend.py
+++ test_backend.py
@@ -13,8 +13,8 @@ def views():
   return viewsCounter('testNetflixViewingHistory.csv')
 
 @pytest.fixture
-def popularity():
-  return popularityCounter(data())
+def popularity(data):
+  return popularityCounter(data)
 
 def test_pickleData(data):
   #print(f"{bcolors.BOLD}Columns in the merged data file should be 'primaryTitle', 'averageRating', 'numVotes', 'Title', 'show_type', 'runtimeMinutes', 'genres', 'tconst', 'Date', 'country'")
@@ -27,3 +27,11 @@ def test_mostViewed(views):
   assert views['High School Musical 2'] == 3, "High School Musical 2 should have 3 view counts."
   assert views['High School Musical'] == 2, "High School Musical should have 2 view counts."
   assert views['Jumanji'] == 1, "Jumanji should have 1 view count."
+
+def test_mostPopular(popularity):
+  assert type(popularity) is dict
+  assert type(popularity['percents']) is list, "Popularity should have percents list."
+  assert len(popularity['percents']) == 3, "Popularity should have percents list of length 3."
+  assert 'Jumanji' in popularity['topShow'], "Jumanji should be in the topShow list."
+  assert 'High School Musical' in popularity['topShow'], "High School Musical should be in the topShow list."
+  assert len(popularity['bottomShow']) == 0, "There should be no shows in the bottomShow list."
