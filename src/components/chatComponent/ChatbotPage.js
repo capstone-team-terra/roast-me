99a6b3e96ebd5dae287faa45485de07c7d8128f9
alisasu <alisa.su97@gmail.com>
2021-04-02 15:21:46 -0400

    FE: added button to home page from allresults

--- src/components/chatComponent/ChatbotPage.js
+++ src/components/chatComponent/ChatbotPage.js
@@ -44,140 +44,140 @@ class ChatbotPage extends React.Component {
       .map(dateHour => dateHour[1])
       .reduce((a, b) => a + b, 0)
     const steps = [
-      // {
-      //   id: '1',
-      //   message: "Hi, I'm an A.I. trained to evaluate Netflix taste.",
-      //   trigger: '2',
-      //   delay: 3000
-      // },
-      // {
-      //   id: '2',
-      //   message:
-      //     'Listen... I have been looking at your Netflix viewing history and um...',
-      //   trigger: '3',
-      //   delay: 3000
-      // },
-      // {
-      //   id: '3',
-      //   message: '...it’s something.',
-      //   trigger: '4',
-      //   delay: 3000
-      // },
-
-      // {
-      //   id: '4',
-      //   message: `I saw  ${sorted[0][1]} ${sorted[0][0]} shows...
-      //   Also, did you really watch ${viewsSorted[0][1]} episodes of ${
-      //     viewsSorted[0][0]
-      //   }?`,
-      //   trigger: '5',
-      //   delay: 4000
-      // },
-      // {
-      //   id: '5',
-      //   options: [
-      //     {
-      //       value: 1,
-      //       label: 'Yes.',
-      //       trigger: '6'
-      //     },
-      //     {
-      //       value: 2,
-      //       label: 'No.',
-      //       trigger: '8'
-      //     },
-      //     {
-      //       value: 3,
-      //       label: 'I share this profile with someone else.',
-      //       trigger: '8'
-      //     }
-      //   ]
-      // },
-      // {
-      //   id: '6',
-      //   message: 'Like ironically?',
-      //   trigger: '7',
-      //   delay: 2000
-      // },
-      // {
-      //   id: '7',
-      //   options: [
-      //     {
-      //       value: 1,
-      //       label: 'Lol yea.',
-      //       trigger: '8'
-      //     },
-      //     {
-      //       value: 2,
-      //       label: 'No...',
-      //       trigger: '9'
-      //     }
-      //   ]
-      // },
-      // {
-      //   id: '8',
-      //   message: `I can tell you are the type to run away from your responsibilities and problems based on how others view you.`,
-      //   trigger: '10',
-      //   delay: 4000
-      // },
-      // {
-      //   id: '9',
-      //   message: "Look i'm just a neural net do what you gotta do",
-      //   trigger: '10',
-      //   delay: 3000
-      // },
-      // {
-      //   id: '10',
-      //   message: `You watched ${totalEpisodes} vids on Netflix.`,
-      //   trigger: '11',
-      //   delay: 3000
-      // },
-      // {
-      //   id: '11',
-      //   message: `Switching topics entirely, how’s your social life?`,
-      //   trigger: '12',
-      //   delay: 4000
-      // },
-      // {
-      //   id: '12',
-      //   options: [
-      //     {value: 1, label: 'What do you think?', trigger: '13'},
-      //     {value: 2, label: 'I have friends.', trigger: '14'}
-      //   ]
-      // },
-      // {
-      //   id: '13',
-      //   message: `I think we both know.`,
-      //   trigger: '15',
-      //   delay: 3000
-      // },
-      // {
-      //   id: '14',
-      //   message: `Not F.R.I.E.N.D.S., but /real/ friends.`,
-      //   trigger: '15',
-      //   delay: 3000
-      // },
-      // {
-      //   id: '15',
-      //   message: `Friends are important.`,
-      //   trigger: '16',
-      //   delay: 3000
-      // },
-      // {
-      //   id: '16',
-      //   message: `But not as important as my opinion.`,
-      //   trigger: '17',
-      //   delay: 3000
-      // },
-      // {
-      //   id: '17',
-      //   message: `Ready to face your truth?`,
-      //   trigger: '18',
-      //   delay: 3000
-      // },
-
       {
         id: '1',
+        message: "Hi, I'm an A.I. trained to evaluate Netflix taste.",
+        trigger: '2',
+        delay: 3000
+      },
+      {
+        id: '2',
+        message:
+          'Listen... I have been looking at your Netflix viewing history and um...',
+        trigger: '3',
+        delay: 3000
+      },
+      {
+        id: '3',
+        message: '...it’s something.',
+        trigger: '4',
+        delay: 3000
+      },
+
+      {
+        id: '4',
+        message: `I saw  ${sorted[0][1]} ${sorted[0][0]} shows...
+        Also, did you really watch ${viewsSorted[0][1]} episodes of ${
+          viewsSorted[0][0]
+        }?`,
+        trigger: '5',
+        delay: 4000
+      },
+      {
+        id: '5',
+        options: [
+          {
+            value: 1,
+            label: 'Yes.',
+            trigger: '6'
+          },
+          {
+            value: 2,
+            label: 'No.',
+            trigger: '8'
+          },
+          {
+            value: 3,
+            label: 'I share this profile with someone else.',
+            trigger: '8'
+          }
+        ]
+      },
+      {
+        id: '6',
+        message: 'Like ironically?',
+        trigger: '7',
+        delay: 2000
+      },
+      {
+        id: '7',
+        options: [
+          {
+            value: 1,
+            label: 'Lol yea.',
+            trigger: '8'
+          },
+          {
+            value: 2,
+            label: 'No...',
+            trigger: '9'
+          }
+        ]
+      },
+      {
+        id: '8',
+        message: `I can tell you are the type to run away from your responsibilities and problems based on how others view you.`,
+        trigger: '10',
+        delay: 4000
+      },
+      {
+        id: '9',
+        message: "Look i'm just a neural net do what you gotta do",
+        trigger: '10',
+        delay: 3000
+      },
+      {
+        id: '10',
+        message: `You watched ${totalEpisodes} vids on Netflix.`,
+        trigger: '11',
+        delay: 3000
+      },
+      {
+        id: '11',
+        message: `Switching topics entirely, how’s your social life?`,
+        trigger: '12',
+        delay: 4000
+      },
+      {
+        id: '12',
+        options: [
+          {value: 1, label: 'What do you think?', trigger: '13'},
+          {value: 2, label: 'I have friends.', trigger: '14'}
+        ]
+      },
+      {
+        id: '13',
+        message: `I think we both know.`,
+        trigger: '15',
+        delay: 3000
+      },
+      {
+        id: '14',
+        message: `Not F.R.I.E.N.D.S., but /real/ friends.`,
+        trigger: '15',
+        delay: 3000
+      },
+      {
+        id: '15',
+        message: `Friends are important.`,
+        trigger: '16',
+        delay: 3000
+      },
+      {
+        id: '16',
+        message: `But not as important as my opinion.`,
+        trigger: '17',
+        delay: 3000
+      },
+      {
+        id: '17',
+        message: `Ready to face your truth?`,
+        trigger: '18',
+        delay: 3000
+      },
+
+      {
+        id: '18',
         component: (
           <div>
             <Button
--- src/components/resultsComponent/AllResults.js
+++ src/components/resultsComponent/AllResults.js
@@ -247,10 +247,11 @@ export class AllResults extends React.Component {
                     Click Here
                   </a>
                 </div>
-                <div>
+                <div style={{position: "fixed", left: "1vw", bottom: "2vh"}}>
+                  <a href="https://roastflix-309106.uk.r.appspot.com/" style={{color: "white"}}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" className="bi bi-caret-left" viewBox="0 0 16 16">
   <path d="M10 12.796V3.204L4.519 8 10 12.796zm-.659.753-5.48-4.796a1 1 0 0 1 0-1.506l5.48-4.796A1 1 0 0 1 11 3.204v9.592a1 1 0 0 1-1.659.753z"/>
-</svg></div>
+</svg></a></div>
               </div>
             </Container>
           ) : (
