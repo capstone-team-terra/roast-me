a9d59c61c212812b547ba0b16cac3dea83a5738c
Danny Serrano <danser829@gmail.com>
2021-04-02 18:43:49 -0400

    FE: submit score on see stats

--- src/components/resultsComponent/Leaderboard.js
+++ src/components/resultsComponent/Leaderboard.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Card, Button} from 'react-bootstrap'
+import {Card} from 'react-bootstrap'
 import ChartCard from './Styling/ChartTheme'
 import { MDBDataTable } from 'mdbreact';
 
@@ -7,14 +7,14 @@ import { MDBDataTable } from 'mdbreact';
 export default function Leaderboard(props) {
   const {leaderboard, username} = props
   let ranking = 0
-
+  console.log('leaderboard', leaderboard)
   const leaderRows = []
   for (let i = leaderboard.length - 1; i >= 0; i--) { // 3
     leaderRows.push({id: leaderboard.length - i, username: leaderboard[i].username, score: leaderboard[i].score})
     if (leaderRows[leaderRows.length - 1]['username'] === username)
       ranking = leaderRows[leaderRows.length - 1]['id']
   }
-
+  // rgba(255, 255, 255, 0.884)
     const data = {
       columns: [
         {
@@ -45,24 +45,24 @@ export default function Leaderboard(props) {
         <p>Users are ranked from most to least basic. The higher the score, the more questionable your taste is.</p>
         {username.length > 0 ? (
           <p>
-          Congratulations, <span style={{color: 'rgba(82, 179, 217, 1)'}}>{username}</span>! You are <span style={{color: 'rgba(234, 87, 102)'}}>#{ranking}{' '}</span> in our leaderboard!
+          Congratulations, <span style={{color: 'rgba(82, 179, 217, 1)'}}>{username}</span>! You are <span style={{color: 'rgba(234, 87, 102)'}}>#{ranking}{' '}</span> on our leaderboard!
           <br/>
           I would be embarassed if I were you.
         </p>
         ) : (
-          <p>Refresh and submit your data to be part of our leaderboard</p>
+          <p>Return to Home and submit your data to be part of our leaderboard</p>
         )}
           <MDBDataTable
           scrollY
-          maxHeight="500px"
+          maxHeight="300px"
           striped
           bordered
           small
           hover
-          className='table-dark'
+          className='table-light square border border-danger'
           data={data}
           paging={false}
-          style={{color: 'rgba(255, 255, 255, 0.884)'}}
+          style={{color: '#db0000'}}
         />
       </Card>
     </ChartCard>
