a972827f1e6a09bbbd917155194b68032589f837
Natalie Rojas <natalie.rojas113@gmail.com>
2021-04-02 22:49:29 -0400

    FE: leaderboard color updated + text size adjusted

--- src/components/resultsComponent/Leaderboard.js
+++ src/components/resultsComponent/Leaderboard.js
@@ -42,9 +42,9 @@ export default function Leaderboard(props) {
         <Card.Title style={{textAlign: 'center', fontSize: 30}}>
           Leaderboard
         </Card.Title>
-        <p>Users are ranked from most to least basic. The higher the score, the more questionable your taste is.</p>
+        <p style={{fontSize: '20px', fontStyle:"italic"}}>Users are ranked from most to least basic. The higher the score, the more questionable your taste is.</p>
         {username.length > 0 ? (
-          <p>
+          <p style={{fontSize: '23px'}}>
           Congratulations, <span style={{color: 'rgba(82, 179, 217, 1)'}}>{username}</span>! You are <span style={{color: 'rgba(234, 87, 102)'}}>#{ranking}{' '}</span> on our leaderboard!
           <br/>
           I would be embarassed if I were you.
@@ -59,10 +59,12 @@ export default function Leaderboard(props) {
           bordered
           small
           hover
-          className='table-light square border border-danger'
+          className='table-dark'
+          // className='table-light square border border-danger'
           data={data}
           paging={false}
-          style={{color: '#db0000'}}
+          style={{color: 'rgba(255, 255, 255, 0.884)'}}
+          // style={{color: '#db0000'}}
         />
       </Card>
     </ChartCard>
